a107413558d057fe9cea4437ebfc418fb4d6d93a
Hannes Achleitner <hannes.software@gmx.at>
2019-03-22 22:21:42 +0100

    Comment in Room

---
+++ app/src/main/java/info/mx/tracks/room/entity/TrackStage.kt
@@ -0,0 +1,16 @@
+package info.mx.tracks.room.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import info.mx.tracks.room.DBMigrationUtil
+
+@Entity(tableName = DBMigrationUtil.TABLE_TRACKSTAGE, indices = arrayOf(Index(value = ["trackId"], name = "TrackStageTrackIdIndex")))
+data class TrackStage(
+        @ColumnInfo(name = "trackId") var trackId: Long = 0,
+        @ColumnInfo(name = "updated") var updated: Long = 0
+) : Track() {
+    override fun toString(): String {
+        return id.toString() + ":" + changed.toString()
+    }
+}
