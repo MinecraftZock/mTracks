cc621a0952f61b372b303902865d97c7f9fc3cea
Hannes Achleitner <hannes.software@gmx.at>
2021-11-20 14:45:21 +0100

    ViewModel

--- MXApp/src/main/java/info/mx/tracks/koin/appModelDI.kt
+++ MXApp/src/main/java/info/mx/tracks/koin/appModelDI.kt
@@ -4,6 +4,8 @@ import info.mx.tracks.data.DataManagerApp
 import info.mx.tracks.navigation.AppNavigationMenu
 import info.mx.tracks.tools.AddMobHelper
 import info.mx.tracks.tools.PermissionHelper
+import info.mx.tracks.trackdetail.comment.CommentRepository
+import info.mx.tracks.trackdetail.comment.CommentViewModel
 import org.koin.dsl.module
 
 val appModule = module {
@@ -11,6 +13,8 @@ val appModule = module {
     single { PermissionHelper(get()) }
     single { AppNavigationMenu() }
     single { AddMobHelper(get()) }
+    single { CommentViewModel() }
+    single { CommentRepository() }
 
     single { DataManagerApp(get()) }
 
