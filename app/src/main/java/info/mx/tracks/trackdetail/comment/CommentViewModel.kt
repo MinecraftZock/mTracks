4d96f952ebe083f1c94e587c9a2acda3d105b579
Hannes Achleitner <hannes.software@gmx.at>
2021-11-24 13:32:37 +0100

    Architecture change in Comment without Rx

--- app/src/main/java/info/mx/tracks/trackdetail/comment/CommentViewModel.kt
+++ app/src/main/java/info/mx/tracks/trackdetail/comment/CommentViewModel.kt
@@ -1,14 +1,26 @@
 package info.mx.tracks.trackdetail.comment
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import info.mx.tracks.room.entity.Comment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import kotlin.coroutines.CoroutineContext
 
 class CommentViewModel() : ViewModel(), KoinComponent {
 
     private val repository: CommentRepository by inject()
+
+    private val coroutineContext: CoroutineContext = Dispatchers.IO
+    private val scope = CoroutineScope(coroutineContext + SupervisorJob())
 
     // Using LiveData and caching what allCommentsByTrack returns has several benefits:
     // - We can put an observer on the data (instead of polling for changes) and only update the
@@ -21,5 +33,31 @@ class CommentViewModel() : ViewModel(), KoinComponent {
      */
     fun insert(word: Comment) = viewModelScope.launch {
         repository.insert(word)
+    }
+
+    fun getNewRemoteData(restId: Long) = viewModelScope.launch {
+        scope.launch {
+            repository.getNewRemoteData(restId)
+        }
+    }
+
+    private fun commentExistsAsync(comment: Comment, commentText: String) =
+        viewModelScope.async { //CoroutineScope(Dispatchers.IO).async or scope.async
+            return@async repository.commentExists(comment.trackId, comment.androidid, commentText)
+        }
+
+    private fun commentExists(comment: Comment, commentText: String): Int = runBlocking {
+        commentExistsAsync(comment, commentText)
+            .await()
+    }
+
+    fun addComment(comment: Comment, commentText: String): Int {
+        val count = commentExists(comment, commentText)
+        if (count == 0)
+            scope.launch {
+                repository.insertCommentsAll(comment)
+                repository.pushCommentsAllNonPushed()
+            }
+        return count
     }
 }
