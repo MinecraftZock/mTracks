27d96c37c2619acd9324a72dd1bdd9ef5399c48b
Hannes Achleitner <hannes.software@gmx.at>
2021-11-24 13:32:37 +0100

    Architecture change in Comment without Rx

--- MXApp/src/main/java/info/mx/tracks/koin/restModelDI.kt
+++ MXApp/src/main/java/info/mx/tracks/koin/restModelDI.kt
@@ -3,7 +3,6 @@ package info.mx.tracks.koin
 import info.mx.comlib.retrofit.CommApiClient
 import info.mx.tracks.MxCoreApplication
 import info.mx.tracks.rest.AppApiClient
-import info.mx.tracks.rest.DataManagerApp
 import org.koin.dsl.module
 
 val restModule = module {
@@ -13,9 +12,6 @@ val restModule = module {
     }
     single {
         AppApiClient(MxCoreApplication.logLevel)
-    }
-    single {
-        DataManagerApp(get(), get())
     }
 
 }
