9a5a88d50f4cfc54722768dcfe2abf16371e3e0c
Hannes Achleitner <hannes.software@gmx.at>
2022-01-22 18:52:44 +0100

    Network by Room

--- app/src/main/java/info/mx/tracks/ops/OpSyncFromServerOperation.kt
+++ app/src/main/java/info/mx/tracks/ops/OpSyncFromServerOperation.kt
@@ -41,6 +41,7 @@ import info.mx.tracks.common.ImportStatusMessage
 import info.mx.tracks.data.DataManagerApp
 import info.mx.tracks.prefs.MxPreferences
 import info.mx.tracks.rest.*
+import info.mx.tracks.room.MxDatabase
 import info.mx.tracks.sqlite.*
 import info.mx.tracks.sqlite.MxInfoDBContract.*
 import info.mx.tracks.util.LocationHelper
@@ -64,6 +65,7 @@ class OpSyncFromServerOperation : AbstractOpSyncFromServerOperation(), KoinCompo
     private lateinit var operationContext: OperationContext
 
     private val dataManagerApp: DataManagerApp by inject()
+    private val mxDatabase: MxDatabase by inject()
 
     /**
      * Gets addresses from location using modern API (Android 13+) or legacy API (older versions).
@@ -345,12 +347,12 @@ class OpSyncFromServerOperation : AbstractOpSyncFromServerOperation(), KoinCompo
     }
 
     private fun doPushNetworkErrorsAtOnce(webClient: MxInfo) {
-        val networkErrors = SQuery.newQuery().select<NetworkRecord>(Network.CONTENT_URI)
+        val networkErrors = mxDatabase.networkDao().all
 
         val requestList = ArrayList<RESTnetworkError>()
         for (networkError in networkErrors) {
             val requestSingle = RESTnetworkError()
-            requestSingle.changed = networkError.created
+            requestSingle.changed = networkError.changed
             requestSingle.reason = networkError.reason
             requestSingle.tracks = networkError.tracks.toInt()
             requestSingle.androidid = TrackingApplication.androidId
@@ -362,14 +364,13 @@ class OpSyncFromServerOperation : AbstractOpSyncFromServerOperation(), KoinCompo
             resultTrack = webClient.postNetworkErrors(requestNetwork)
             if (resultTrack.responseCode == Response.HTTP_NO_CONTENT) {
                 for (networkError in networkErrors) {
-                    networkError.delete(false)
+                    mxDatabase.networkDao().deleteAll()
                 }
             } else {
                 Timber.w("Network result code:%s", resultTrack.responseCode)
             }
         } catch (_: ServiceException) {
         }
-
     }
 
     private fun doFixMissingCounty(context: Context) {
