683bfb11a82c42faf4cb92b5a756952a9190c4c6
Hannes Achleitner <hannes.software@gmx.at>
2022-01-22 18:14:55 +0100

    Import into Room database

--- app/src/main/java/info/mx/tracks/room/MxDatabase.kt
+++ app/src/main/java/info/mx/tracks/room/MxDatabase.kt
@@ -3,7 +3,6 @@ package info.mx.tracks.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import info.mx.tracks.room.entity.*
-import info.mx.tracks.room.memory.NetworkDao
 
 @Database(entities = [CapturedLatLng::class, Comment::class, Country::class, Event::class,
     Favorit::class, Network::class, Picture::class, Route::class, Series::class, Track::class,
@@ -17,6 +16,14 @@ abstract class MxDatabase : RoomDatabase() {
     abstract fun eventDao(): EventDao
 
     abstract fun networkDao(): NetworkDao
+
+    abstract fun trackDao(): TrackDao
+
+    abstract fun trackStageDao(): TrackStageDao
+
+    abstract fun pictureDao(): PictureDao
+
+    abstract fun countryDao(): CountryDao
 
     companion object {
 
