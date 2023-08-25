16a0aa63f3a7c68895b2e13b9d862eae0dc8df6f
Hannes Achleitner <hannes.software@gmx.at>
2023-08-25 13:35:43 +0200

    Publish it

--- signing/decrypt.sh
+++ signing/decrypt.sh
@@ -21,6 +21,7 @@ pushd signing
 # to encrypt
 #openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in ./signing/release.keystore -out release.keystore.enc
 #openssl aes-256-cbc -salt -pbkdf2 -k "$CRYPT_PASS" -in ./MXApp/google-services.json -out ./MXApp/google-services.json.enc
+#openssl aes-256-cbc -salt -pbkdf2 -k "$CRYPT_PASS" -in ./signing/Surveilance-playstore.json -out ./signing/Surveilance-playstore.json.enc
 
 # Ubuntu 18.04 (openssl 1.1.0g+) needs -md md5
 # https://askubuntu.com/questions/1067762/unable-to-decrypt-text-files-with-openssl-on-ubuntu-18-04/1076708
@@ -30,5 +31,8 @@ openssl aes-256-cbc -a -d -pbkdf2 -k "$CRYPT_PASS" -in release.keystore.enc -out
 echo google-services.json
 echo ====================
 openssl aes-256-cbc -d -pbkdf2 -k "$CRYPT_PASS" -in ../MXApp/google-services.json.enc -out ../MXApp/google-services.json
+echo Surveilance-playstore.json
+echo ====================
+openssl aes-256-cbc -d -pbkdf2 -k "$CRYPT_PASS" -in Surveilance-playstore.json.enc -out Surveilance-playstore.json
 
 popd 1>/dev/null
