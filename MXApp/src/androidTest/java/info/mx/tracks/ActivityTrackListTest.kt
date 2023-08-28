d51ef16f72777e641ab7f56e08267c843c75889a
Hannes Achleitner <hannes.software@gmx.at>
2023-08-28 21:22:49 +0200

    Wait for import in Espresso

--- MXApp/src/androidTest/java/info/mx/tracks/ActivityTrackListTest.kt
+++ MXApp/src/androidTest/java/info/mx/tracks/ActivityTrackListTest.kt
@@ -4,16 +4,22 @@ import android.Manifest
 import androidx.test.core.graphics.writeToTestStorage
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingPolicies
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
+import info.mx.tracks.ops.CountingIdlingResourceSingleton
 import info.mx.tracks.tracklist.ActivityTrackList
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestName
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
 
 
 @RunWith(AndroidJUnit4::class)
@@ -24,6 +30,17 @@ class ActivityTrackListTest {
 
     @get:Rule
     var nameRule = TestName()
+
+    @Before
+    fun registerIdlingResource() {
+        IdlingPolicies.setIdlingResourceTimeout(4, TimeUnit.MINUTES)
+        IdlingRegistry.getInstance().register(CountingIdlingResourceSingleton.countingIdlingResource)
+    }
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(CountingIdlingResourceSingleton.countingIdlingResource)
+    }
 
     @get:Rule
     val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
