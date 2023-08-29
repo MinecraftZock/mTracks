1b569e2b53d2641a9200ec9daac4e47850d763b1
Hannes Achleitner <hannes.software@gmx.at>
2023-08-29 12:56:17 +0200

    Use debug.keystore
    This is a pre-condition to screenshot map in Espresso tests

--- signing/decrypt.sh
+++ signing/decrypt.sh
@@ -19,20 +19,27 @@ openssl version
 pushd signing
 
 # to encrypt
-#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in ./signing/release.keystore -out release.keystore.enc
+#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in ./signing/release.keystore -out ./signing/release.keystore.enc
+#openssl aes-256-cbc -salt -pbkdf2 -k "$CRYPT_PASS" -in ~/.android/debug.keystore -out ./signing/debug.keystore.enc
 #openssl aes-256-cbc -salt -pbkdf2 -k "$CRYPT_PASS" -in ./MXApp/google-services.json -out ./MXApp/google-services.json.enc
 #openssl aes-256-cbc -salt -pbkdf2 -k "$CRYPT_PASS" -in ./signing/Surveilance-playstore.json -out ./signing/Surveilance-playstore.json.enc
+
+# shellcheck disable=SC2038
+find . -name "*.keystore.enc" | xargs ls -la
 
 # Ubuntu 18.04 (openssl 1.1.0g+) needs -md md5
 # https://askubuntu.com/questions/1067762/unable-to-decrypt-text-files-with-openssl-on-ubuntu-18-04/1076708
 echo release.keystore
-echo ================
 openssl aes-256-cbc -a -d -pbkdf2 -k "$CRYPT_PASS" -in release.keystore.enc -out release.keystore
+echo debug.keystore
+openssl aes-256-cbc -d -pbkdf2 -k "$CRYPT_PASS" -in debug.keystore.enc -out debug.keystore
+
+# shellcheck disable=SC2038
+find . -name "*.keystore" | xargs ls -la
+
 echo google-services.json
-echo ====================
 openssl aes-256-cbc -d -pbkdf2 -k "$CRYPT_PASS" -in ../MXApp/google-services.json.enc -out ../MXApp/google-services.json
 echo Surveilance-playstore.json
-echo ====================
 openssl aes-256-cbc -d -pbkdf2 -k "$CRYPT_PASS" -in Surveilance-playstore.json.enc -out Surveilance-playstore.json
 
 popd 1>/dev/null
