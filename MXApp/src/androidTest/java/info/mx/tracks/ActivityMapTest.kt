a29109b4efc453e05b3c77ec0aca21f39d4db2d6
Hannes Achleitner <hannes.software@gmx.at>
2023-08-30 09:46:13 +0200

    Map test

---
+++ MXApp/src/androidTest/java/info/mx/tracks/ActivityMapTest.kt
@@ -0,0 +1,28 @@
+package info.mx.tracks
+
+import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.screenshot.captureToBitmap
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import info.mx.tracks.map.ActivityMapExtension
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class ActivityMapTest: BaseSyncTest() {
+
+    @get:Rule
+    val activityScenarioRule = activityScenarioRule<ActivityMapExtension>()
+
+    @Test
+    fun smokeTestSimplyStart() {
+        Espresso.pressBack()
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-2")
+    }
+
+}
