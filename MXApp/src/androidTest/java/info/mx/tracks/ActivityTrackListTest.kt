a13751763a02458d206349f92470a83206317c8b
Hannes Achleitner <hannes.software@gmx.at>
2023-09-18 07:42:08 +0200

    No screenshot from frist-time-settings screen

--- MXApp/src/androidTest/java/info/mx/tracks/ActivityTrackListTest.kt
+++ MXApp/src/androidTest/java/info/mx/tracks/ActivityTrackListTest.kt
@@ -32,7 +32,8 @@ class ActivityTrackListTest {
 
     @Test
     fun smokeTestSimplyStart() {
-        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-1")
+        // This is the first time settings activity with always changed version number
+        //onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-1")
         Espresso.pressBack()
         Thread.sleep(4000)
         onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-2")
