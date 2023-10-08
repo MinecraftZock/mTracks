55b9b8d8cdd0129c580f028e78be2822cba3edd3
Hannes Achleitner <hannes.software@gmx.at>
2023-10-08 17:49:26 +0200

    Track filter test

---
+++ MXApp/src/androidTest/java/info/mx/tracks/ActivityFilterCountryTest.kt
@@ -0,0 +1,36 @@
+package info.mx.tracks
+
+import android.Manifest
+import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.screenshot.captureToBitmap
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
+import info.mx.tracks.settings.ActivityFilterCountry
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class ActivityFilterCountryTest : BaseSyncTest() {
+
+    @get:Rule
+    val activityScenarioRule = activityScenarioRule<ActivityFilterCountry>()
+
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    @Test
+    fun smokeTestSimplyStart() {
+        // This is the first time settings activity with always changed version number
+        //onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-1")
+//        Espresso.pressBack()
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-2")
+    }
+
+}
