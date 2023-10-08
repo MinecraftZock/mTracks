675fbaa7f1e078dbd38e64722e63be0ff6011c82
Hannes Achleitner <hannes.software@gmx.at>
2023-10-08 17:49:26 +0200

    Setting test

--- MXApp/src/androidTest/java/info/mx/tracks/ActivitySettingTest.kt
+++ MXApp/src/androidTest/java/info/mx/tracks/ActivitySettingTest.kt
@@ -2,24 +2,23 @@ package info.mx.tracks
 
 import android.Manifest
 import androidx.test.core.graphics.writeToTestStorage
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
-import info.mx.tracks.tracklist.ActivityTrackList
+import info.mx.tracks.settings.ActivitySetting
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
-class ActivityTrackListTest : BaseSyncTest() {
+class ActivitySettingTest : BaseSyncTest() {
 
     @get:Rule
-    val activityScenarioRule = activityScenarioRule<ActivityTrackList>()
+    val activityScenarioRule = activityScenarioRule<ActivitySetting>()
 
     @get:Rule
     val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
@@ -30,7 +29,7 @@ class ActivityTrackListTest : BaseSyncTest() {
     fun smokeTestSimplyStart() {
         // This is the first time settings activity with always changed version number
         //onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-1")
-        Espresso.pressBack()
+//        Espresso.pressBack()
         onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-2")
     }
 
