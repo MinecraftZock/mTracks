8e0cd6cfeab9142e508757053541f09920d2fb79
Hannes Achleitner <hannes.software@gmx.at>
2023-10-09 07:56:11 +0200

    More tests

--- MXApp/src/androidTest/java/info/mx/tracks/ActivityFilterCountryTest.kt
+++ MXApp/src/androidTest/java/info/mx/tracks/ActivityFilterCountryTest.kt
@@ -1,14 +1,21 @@
 package info.mx.tracks
 
 import android.Manifest
+import android.widget.ListView
 import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeDown
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
 import info.mx.tracks.settings.ActivityFilterCountry
+import org.hamcrest.Matchers.anything
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,6 +38,13 @@ class ActivityFilterCountryTest : BaseSyncTest() {
         //onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-1")
 //        Espresso.pressBack()
         onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-2")
+        onView(withId(android.R.id.list)).perform(swipeDown())
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-3")
+        onData(anything())
+            .inAdapterView(isAssignableFrom(ListView::class.java))
+            .atPosition(3)
+            .perform(click())
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-4")
     }
 
 }
