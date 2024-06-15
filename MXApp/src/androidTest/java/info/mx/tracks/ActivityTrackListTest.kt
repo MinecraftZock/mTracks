d052d935d16af8791f6a8f7f50af08c4b2c86131
Hannes Achleitner <hannes.software@gmx.at>
2024-06-15 10:33:01 +0200

    Search tracks test

--- MXApp/src/androidTest/java/info/mx/tracks/ActivityTrackListTest.kt
+++ MXApp/src/androidTest/java/info/mx/tracks/ActivityTrackListTest.kt
@@ -4,7 +4,11 @@ import android.Manifest
 import androidx.test.core.graphics.writeToTestStorage
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.clearText
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -34,4 +38,40 @@ class ActivityTrackListTest : BaseSyncTest() {
         onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-init")
     }
 
+    @Test
+    fun searchTest() {
+        // This is the first time settings activity with always changed version number
+//        Espresso.pressBack()
+        onView(withId(R.id.menu_search)).perform(click())
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-search-click")
+
+        onView(withId(R.id.search_src_text)).perform(clearText(), typeText("aa"))
+        Thread.sleep(WAIT_EDIT)
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-search-aa")
+        onView(withId(R.id.search_src_text)).perform(clearText(), typeText("de"))
+        Thread.sleep(WAIT_EDIT)
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-search-de")
+        onView(withId(R.id.search_src_text)).perform(clearText(), typeText("ee"))
+        Thread.sleep(WAIT_EDIT)
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-search-ee")
+        onView(withId(R.id.search_src_text)).perform(clearText(), typeText("ck"))
+        Thread.sleep(WAIT_EDIT)
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-search-ck")
+        onView(withId(R.id.search_src_text)).perform(clearText(), typeText("sch"))
+        Thread.sleep(WAIT_EDIT)
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-search-sch")
+        onView(withId(R.id.search_src_text)).perform(clearText(), typeText("oo"))
+        Thread.sleep(WAIT_EDIT)
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-search-oo")
+        onView(withId(R.id.search_src_text)).perform(clearText(), typeText("tz"))
+        Thread.sleep(WAIT_EDIT)
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-search-tz")
+        onView(withId(R.id.search_src_text)).perform(clearText(), typeText("ff"))
+        Thread.sleep(WAIT_EDIT)
+        onView(isRoot()).captureToBitmap().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-search-ff")
+    }
+
+    companion object {
+        const val WAIT_EDIT = 200L
+    }
 }
