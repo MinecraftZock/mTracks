922aba6563949818e091796fec3a6fc21fb2b059
Hannes Achleitner <hannes.software@gmx.at>
2024-08-16 18:39:10 +0200

    Map detail test

---
+++ app/src/androidTest/java/info/mx/tracks/MapArcoTest.kt
@@ -0,0 +1,9 @@
+package info.mx.tracks
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import info.mx.tracks.base.BaseMapTest
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class MapArcoTest : BaseMapTest(4074L, true)
