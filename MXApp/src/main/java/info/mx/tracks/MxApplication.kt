8bb0aab60fb066411d72afd6c257fcca75139131
Hannes Achleitner <hannes.software@gmx.at>
2024-08-26 14:09:43 +0200

    Debug is no admin

--- MXApp/src/main/java/info/mx/tracks/MxApplication.kt
+++ MXApp/src/main/java/info/mx/tracks/MxApplication.kt
@@ -61,7 +61,7 @@ open class MxApplication : MxCoreApplication(), KoinComponent {
 
     override fun onCreate() {
         isDebug = BuildConfig.DEBUG
-        isAdmin = isDebug
+//        isAdmin = isDebug
         showWeather = BuildConfig.SHOW_WEATHER
 
         // for 15 minutes disable admin
