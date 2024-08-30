29b06f062242dedb2695228a7a38c8038fba0284
Hannes Achleitner <hannes.software@gmx.at>
2024-08-30 07:50:21 +0200

    Cosmetic

--- MXApp/src/main/java/info/mx/tracks/service/LocationRecalculateService.kt
+++ MXApp/src/main/java/info/mx/tracks/service/LocationRecalculateService.kt
@@ -4,19 +4,19 @@ import android.content.Context
 import android.content.Intent
 import android.location.Location
 import androidx.core.app.JobIntentService
+import info.mx.tracks.common.parcelableExtra
 import info.mx.tracks.ops.OpSyncFromServerOperation.Companion.LOCATION
 import info.mx.tracks.ops.OpSyncFromServerOperation.Companion.SOURCE
 
 class LocationRecalculateService : JobIntentService() {
     override fun onHandleWork(intent: Intent) {
-        val location: Location? = intent.getParcelableExtra(LOCATION)
+        val location: Location? = intent.parcelableExtra(LOCATION)
         val source: String? = intent.getStringExtra(SOURCE)
 
         val recalculateDistance = RecalculateDistance(this)
         location?.let {
             recalculateDistance.recalculateTracks(it, "$source Service")
         }
-
     }
 
     // convenient method for starting the service.
