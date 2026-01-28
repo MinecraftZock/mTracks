23d9860e17e16fc73c9c5ba416a8dbbbf24fa8d8
Hannes Achleitner <hannes.software@gmx.at>
2026-01-28 18:16:45 +0100

    Use lat/lon everywhere

--- app/src/androidTest/java/info/mx/tracks/base/BaseSyncTest.kt
+++ app/src/androidTest/java/info/mx/tracks/base/BaseSyncTest.kt
@@ -3,6 +3,8 @@ package info.mx.tracks.base
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.IdlingPolicies
 import androidx.test.espresso.IdlingRegistry
@@ -11,6 +13,7 @@ import info.mx.core.ops.ImportIdlingResource
 import info.mx.core.ops.RecalculateIdlingResource
 import info.mx.core_generated.prefs.MxPreferences.Companion.PREFERENCES_NAME
 import info.mx.core_generated.prefs.MxPreferences.Keys
+import info.mx.tracks.common.FragmentUpDown
 import info.mx.tracks.map.ActivityMapExtension
 import org.junit.After
 import org.junit.Before
@@ -19,7 +22,7 @@ import org.junit.rules.TestName
 import java.util.concurrent.TimeUnit
 
 
-abstract class BaseSyncTest {
+abstract class BaseSyncTest<T : AppCompatActivity> {
 
     @get:Rule
     var nameRule = TestName()
@@ -31,18 +34,35 @@ abstract class BaseSyncTest {
         IdlingRegistry.getInstance().register(ImportIdlingResource.countingIdlingResource)
         IdlingRegistry.getInstance().register(RecalculateIdlingResource.countingIdlingResource)
         firstTimeImport()
-
-        // Create intent with specific location (example MX track)
-        val intent = Intent(ApplicationProvider.getApplicationContext(), ActivityMapExtension::class.java).apply {
-            putExtra(ActivityMapExtension.LAT, 48.1351) // Example latitude
-            putExtra(ActivityMapExtension.LON, 11.5820) // Example longitude (Munich area)
-        }
     }
 
     @After
     fun unregisterIdlingResource() {
         IdlingRegistry.getInstance().unregister(ImportIdlingResource.countingIdlingResource)
         IdlingRegistry.getInstance().unregister(RecalculateIdlingResource.countingIdlingResource)
+    }
+
+    inline fun <reified A : T> startActivity(
+        lat: Double = 48.1351,
+        lon: Double = 11.5820,
+        contentUri: String? = null,
+        restTrackId: Long = -1L
+    ) {
+        val intent = Intent(ApplicationProvider.getApplicationContext(), A::class.java).apply {
+            if (lat + lon != 0.0) {
+                putExtra(ActivityMapExtension.LAT, lat)
+                putExtra(ActivityMapExtension.LON, lon)
+            }
+            contentUri?.let {
+                putExtra(FragmentUpDown.CONTENT_URI, contentUri)
+            }
+            if (restTrackId > -1)
+                putExtra(FragmentUpDown.RECORD_ID_LOCAL, restTrackId)
+
+        }
+        ActivityScenario.launch<A>(intent).use { _ ->
+            Thread.sleep(2000)
+        }
     }
 
     fun firstTimeImport() {
