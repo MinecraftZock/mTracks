34506ff1b738de0d9fcc33a0b05bc273cbac59d2
Hannes Achleitner <hannes.software@gmx.at>
2026-01-28 10:17:47 +0100

    Code cosmetic

--- commonLib/src/main/java/info/hannes/commonlib/PackageTools.kt
+++ commonLib/src/main/java/info/hannes/commonlib/PackageTools.kt
@@ -4,9 +4,6 @@ import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.os.Build
 
-/**
- * Created by Hannes Achleitner on 02.09.23.
- */
 fun PackageManager.getPackageInfoCompat(packageName: String, flags: Int = 0): PackageInfo =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(flags.toLong()))
